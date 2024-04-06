dfd60f484198b2443f33c8ce05becd5eb2345890
raphaelreynaud-web <contact.raphaelenlive@gmail.com>
2024-04-06 07:38:10 +0200

    import all in pastelUi file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import * as PastelUi from './Components/PastelUI'
 function App() {
   return (
     <>
-      <p>Coucou</p>
+      <PastelUi.ResponsiveContainer $width='1280px' $margin='auto'>
+        <PastelUi.Flex $justifyContent='space-between' $alignItems='center'>
+          <PastelUi.Box $width="100px" $padding='20px' $backgroundColor='#F9F9F9'>Navigation</PastelUi.Box>
+          <PastelUi.Box $width="100px" $backgroundColor='#F9F9F9'>
+            <PastelUi.Button $color='green'>Sign In</PastelUi.Button>
+          </PastelUi.Box>
+        </PastelUi.Flex>
+        <PastelUi.Flex>
+          <PastelUi.Flex $direction='column' $flex='1'>
+            <PastelUi.Box $padding='20px' $height='300px' $backgroundColor='blue'>Sidebar</PastelUi.Box>
+          </PastelUi.Flex>
+          <PastelUi.Flex $direction='column' $flex='3'>
+            <PastelUi.Box $padding='20px' $height='300px' $backgroundColor='#F9F9F9'>Content</PastelUi.Box>
+          </PastelUi.Flex>
+        </PastelUi.Flex>
+      </PastelUi.ResponsiveContainer>
     </>
   )
 }
---
+++ src/Components/PastelUI.tsx
@@ -0,0 +1,23 @@
+import Alert from "./Alert";
+import Box from "./Box";
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import Flex from "./Flex";
+import Grid from "./Grid";
+import ListItem from "./ListItem";
+import OrderedList from "./OrderedList";
+import ResponsiveContainer from "./ResponsiveContainer";
+import UnorderedList from "./UnorderedList";
+
+export {
+    Alert,
+    Box,
+    Button,
+    Checkbox,
+    Flex,
+    Grid,
+    ListItem,
+    OrderedList,
+    ResponsiveContainer,
+    UnorderedList,
+};
